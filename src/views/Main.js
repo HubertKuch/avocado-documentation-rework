16473ed673288cf210b0fecd1d256910198b241c
HubertKuch <kuchhubert@gmail.com>
2023-03-06 15:42:01 +0100

    Main view

--- src/views/Main.js
+++ src/views/Main.js
@@ -4,6 +4,16 @@ function Main() {
   return (
     <div className="App">
         <Header />
+        <section className={"project-section"}>
+            <div>
+                <h1>
+                    Progressive PHP framework
+                </h1>
+                <h4>
+                    for building scalable web apps in PHP, based on drivers and classes
+                </h4>
+            </div>
+        </section>
     </div>
   );
 }
